868eeb59925c42dd090bf94e09d6cc4d58deb530
Dylan <dylan@vandenbosch.co.za>
2018-03-03 14:42:04 +0200

    Connected login and signup components to their wrapper

--- src/components/authentication/AppAuthentication.vue
+++ src/components/authentication/AppAuthentication.vue
@@ -3,7 +3,11 @@
       <v-layout row wrap>
           <v-flex xs6 offset-xs3 justify-center>
               <v-card >
-                <h3>Authentication Component Wrapper</h3>    
+                <h3>Authentication Component Wrapper</h3>
+                <!-- 3) PLACE the registered component into the template
+                notice: CamelCase becomes hyphenated-component -->
+                <log-in/>
+                <sign-up/>   
               </v-card>
           </v-flex>
       </v-layout>
@@ -11,12 +15,20 @@
 </template>
 
 <script>
+// Adding Components is a three-step process
+import LogIn from "./LogIn"; // 1) IMPORT the File
+import SignUp from "./SignUp";
+
 export default {
   data() {
     return {};
   },
   methods: {},
-  components: {}
+  components: {
+    LogIn, // 2) REGISTER the Component
+    SignUp
+  }
 };
+// Notice: Both login and signup form placeholders are now visible, later will make visibility reactive
 </script>
 
