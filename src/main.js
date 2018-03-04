c1e57b23604f71a680899515f8e4cc9f4e1d0121
Dylan <dylan@vandenbosch.co.za>
2018-03-04 17:12:51 +0200

    minor tweaks left after pivot away from vue-feathers library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Include and set up feathers client
+import Feathers from '@feathersjs/client'
+import auth from '@feathersjs/authentication-client'
+import socketio from '@feathersjs/socketio-client'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3030/', {
+  transports: ['websocket'],
+  forceNew: true
+}) // TODO use env variables for production
+const feathers = Feathers()
+  .configure(socketio(socket))
+  .configure(auth({
+    storage: window.localStorage
+  }))
+
+// feathers.service('messages').create({
+//   text: 'A new message'
+// })
+
+// Standard Vue with Vuetify stuff
 import Vue from 'vue'
 import App from './App'
 import router from './router'
